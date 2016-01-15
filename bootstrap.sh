394dff588f42f0a03a67f42603f211e232b8cb4f
John McCormack <mccormack.wgsi@gmail.com>
2016-01-15 13:00:42 -0500

    Updated Bootstrap file, should really start working to make this more dynamic

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,11 +92,43 @@ sudo make install
 
 sudo ldconfig
 
-#TODO
+##Which also needs gr-foo
+cd ..
+cd ..
+git clone https://github.com/bastibl/gr-foo.git
+cd gr-foo
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
+
+#RTL Driver
 cd ..
 cd ..
 git clone git://git.osmocom.org/rtl-sdr.git
-cd 
+cd rtl-sdr
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+sudo make
+sudo make install
+sudo ldconfig
+
+#RTL-GNU Radio Pluging
+cd ..
+cd ..
+git clone git://git.osmocom.org/gr-osmosdr
+cd gr-osmosdr/
+mkdir build
+cd build/
+cmake ../
+make
+sudo make install
+sudo ldconfig
+
 
 
 
