6af50cf0ba64e3c7b21f0fc61d6500c77bc7c88b
John McCormack <mccormack.wgsi@gmail.com>
2016-01-14 19:17:26 -0500

    Added gnome to the vagrant install

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,6 +44,8 @@ apt-get --yes --force-yes install libusb-1.0-0-dev
 apt-get --yes --force-yes install python-mako
 apt-get --yes --force-yes install python-docutils
 apt-get --yes --force-yes install xinitx
+apt-get --yes --force-yes install batctl
+apt-get --yes --force-yes install gnome
 
 sudo pip install pyside
 
