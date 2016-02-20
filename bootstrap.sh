1620297364eb307e86a673707cb84feee14b7ddc
Joe Prine <josephprine@gmail.com>
2016-02-19 19:28:12 -0500

    Initial commit after fork.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,49 +4,13 @@ apt-get update
 
 #Everything below here is mentioned as being a prereq for 
 #GNURadio except Vim, Vim is just swell. Love Vim.
-apt-get --yes --force-yes install python
-apt-get --yes --force-yes install vim
-apt-get --yes --force-yes install cmake
-apt-get --yes --force-yes install python-pip
-apt-get --yes --force-yes install libboost-all-dev
-apt-get --yes --force-yes install libcppunit-dev
-apt-get --yes --force-yes install git
-apt-get --yes --force-yes install libfftw3-dev libfftw3-doc
-apt-get --yes --force-yes install libgsl0ldbl
-apt-get --yes --force-yes install swig
-apt-get --yes --force-yes install libsdl2-2.0
-apt-get --yes --force-yes install python-wxgtk2.8
-apt-get --yes --force-yes install build-essential
-apt-get --yes --force-yes install mesa-common-dev
-apt-get --yes --force-yes install qt-sdk
-apt-get --yes --force-yes install libqwt-dev
-apt-get --yes --force-yes install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
-apt-get --yes --force-yes install python-setuptools
-apt-get --yes --force-yes install python-gtk2-dev
-apt-get --yes --force-yes install doxygen
-apt-get --yes --force-yes install doxygen-gui
-apt-get --yes --force-yes install graphviz
-apt-get --yes --force-yes install git-core
-apt-get --yes --force-yes install g++
-apt-get --yes --force-yes install python-dev
-apt-get --yes --force-yes install pkg-config
-apt-get --yes --force-yes install python-cheetah
-apt-get --yes --force-yes install python-lxml
-apt-get --yes --force-yes install libxi-dev
-apt-get --yes --force-yes install python-sip
-apt-get --yes --force-yes install libqt4-opengl-dev
-apt-get --yes --force-yes install libfontconfig1-dev
-apt-get --yes --force-yes install libxrender-dev
-apt-get --yes --force-yes install python-sip
-apt-get --yes --force-yes install python-sip-dev
-apt-get --yes --force-yes install libboost-all-dev
-apt-get --yes --force-yes install libusb-1.0-0-dev
-apt-get --yes --force-yes install python-mako
-apt-get --yes --force-yes install python-docutils
-apt-get --yes --force-yes install xinitx
-apt-get --yes --force-yes install batctl
-apt-get --yes --force-yes install gnome
-apt-get --yes --force-yes install xinit
+$ sudo dnf install fftw-devel cppunit-devel wxPython-devel libusb-devel \
+  guile boost-devel alsa-lib-devel numpy gsl-devel python-devel pygsl \
+  swig python-sphinx orc-devel orc-compiler doxygen python-docutils \
+  python-cheetah python-lxml guile-devel PyOpenGL PyQt4-devel qwt-devel \
+  qwtplot3d-qt4-devel cmake xmlto graphviz PyQwt PyQwt-devel zeromq2-devel \
+  python-zmq SDL-devel cmake make gcc gcc-c++ boost-devel scipy \
+  thrift-devel python-thrift python-mako gpsd-devel
 
 sudo pip install pyside
 
@@ -56,14 +20,14 @@ export PYTHONPATH=$PYTHONPATH:/usr/bin/python
 
 
 #remove standard packages of UHD if they are there
-sudo apt-get --yes --force-yes remove libuhd-dev
-sudo apt-get --yes --force-yes remove libuhd003
-sudo apt-get --yes --force-yes remove uhd-host
+sudo dnf remove -y libuhd-dev
+sudo dnf remove -y libuhd003
+sudo dnf remove -y uhd-host
 
 #Install UHD Using Ettus Repos
 sudo bash -c 'echo "deb http://files.ettus.com/binaries/uhd/repo/uhd/ubuntu/`lsb_release -cs` `lsb_release -cs` main" > /etc/apt/sources.list.d/ettus.list'
-sudo apt-get update
-sudo apt-get --yes --force-yes install -t `lsb_release -cs` uhd
+sudo dnf upgrade
+sudo dnf install - `lsb_release -cs` uhd
 
 git clone --recursive http://git.gnuradio.org/git/gnuradio.git
 
